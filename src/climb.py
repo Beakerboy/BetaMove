cd59c223821712d699c26430179f3123f46e65ce
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-12 16:01:48 -0400

    Update climb.py

--- src/climb.py
+++ src/climb.py
@@ -1,3 +1,4 @@
+import re
 from typing import TypeVar, Type
 
 
@@ -23,23 +24,43 @@ class Climb:
         # The JSON object has three grades; grade, info[2] and UserGrade
         self._grade = ""
 
+        self._start_holds = 0
+        self._finish_holds = 0
+
     # Setters and Getters
     def set_id(self: T, id: str) -> None:
         self._id = id
+        # validate int as string
 
     def set_name(self: T, name: str) -> None:
         self._name = name
 
-    def add_hold(self: T, hold: dict) -> None:
+    def add_hold(self: T, hold: list) -> None:
+        if hold[1] and hold[2]:
+            raise exception
+        if hold[1] and self._start_holds == 2:
+            raise exception
+        if hold[2] and self._finish_holds == 2:
+            raise exception
+        if self._moves.len() > 14:
+            raise exception
+        if re.match("[A-K]([1-9]|[1][0-8])", hold[0]) != hold[0]:
+            raise exception
         self._moves.append(hold)
+        if (hold[1]):
+            self._start_holds += 1
+        if (hold[2]):
+            self._finish_holds += 1
+        # Validate x and Y
 
     def set_grade(self: T, grade: str) -> None:
         self._grade = grade
+        # Validate acceptible values
 
     def url(self: T) -> str:
         url = "https://moonboard.com/Problems/View/"
         return url + self._id + "/" + self._name.lower()
-
+        
     @classmethod
     def from_json(cls: Type[T], id: int, data: dict) -> T:
         # parse data and set attributes
@@ -55,4 +76,5 @@ class Climb:
                     hold["IsEnd"]
                 ]
             )
+        # Validate number of start, number of end, total holds.
         return climb
