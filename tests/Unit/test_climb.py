2f75b156e962f97fc84f31abe0039ee3e546c059
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-12 19:21:41 -0400

    Update test_climb.py

--- tests/Unit/test_climb.py
+++ tests/Unit/test_climb.py
@@ -9,7 +9,21 @@ def test_constructor() -> None:
 
 
 def test_factory() -> None:
-  
+    data = json.load('{
+        "1": {
+            "grade": "5+",
+            "problem_name": "Foo",
+            "moves": [
+                {"Description": "A1", "IsStart": true, "IsEnd": false},
+                {"Description": "A18", "IsStart": false, "IsEnd": true},
+                {"Description": "A8", "IsStart": false, "IsEnd": false}
+            ]
+        }
+    }')
+    climb = Climb.from_json(data)
+    assert climb.get_name() == "Foo"
+
+
 # Setters and Getters
 
 def test_bad_column() -> None:
