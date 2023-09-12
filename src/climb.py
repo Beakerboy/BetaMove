64f655b6c471ee87ec28b8cb4f637ca34203aaca
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-12 14:14:51 -0400

    Create climb.py

---
+++ src/climb.py
@@ -0,0 +1,41 @@
+from typing import TypeVar
+
+
+T = TypeVar('T', bound='Climb')
+
+
+class Climb:
+
+    # class default constructor
+    def __init__(self: T):
+
+        # Instance Attributes
+        # The moonboard ID
+        def self._id = 0
+
+        # The problem name
+        def self._name = ""
+
+        # The list of allowed holds with start and end
+        def self._moves = []
+
+        # The grade
+        def self._grade = ""
+
+    # Setters and Getters
+    def set_id(self: T, id: int) -> None:
+        self._id = id
+
+    def set_name(self: T, name: str) -> None:
+        self._name = name
+
+    def add_move(self: T, move: dict) -> None:
+        self._moves.append(move)
+
+    def set_grade(self: T, grade: str) -> None:
+        self._grade = grade
+
+    @classmethod
+    def from_json(cls: Type[T], json:str) -> T:
+        # parse data and set attributes
+        return cls
