aa6d0eb4f582de9b21ae0906c59f5eb0a2d0478f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-13 10:39:44 -0400

    Create main.py

---
+++ src/beta_move/main.py
@@ -0,0 +1,37 @@
+from beta_move.beta_move import BetaMove
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename")
+    parser.add_argument("-l", "--left",
+                        help="The left hand difficulty file name.")
+    parser.add_argument("-r", "--right",
+                        help="The right hand difficulty file name.")
+    parser.add_argument("-o", "--output",
+                        help="The output file name.")
+    args = parser.parse_args()
+
+    def transform(file: str) -> dict:
+        features = pd.read_csv(file, dtype=str)
+        dict = {}
+        for index in features.index:
+            item = features.loc[index]
+            dict[
+                (
+                    int(item['X_coord']),
+                    int(item['Y_coord'])
+                )
+            ] = np.array(
+                list(item['Difficulties'])
+            ).astype(int)
+
+    app = BetaMove(transform(args.left), transform(args.right))
+    # Load the json file
+    f = open(args.filename)
+    f_out = open(args.output, "a")
+    data = json.load(f)
+    for id in data:
+        climb = Climb.from_json(id, data[id])
+        movement = app.create_movement(climb)
+        f_out.write(movement)
