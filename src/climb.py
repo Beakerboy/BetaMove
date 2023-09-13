ad85038f1684d7008ccf7dbf71ea7b0717a1b335
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-12 20:08:06 -0400

    Update climb.py

--- src/climb.py
+++ src/climb.py
@@ -48,9 +48,8 @@ class Climb:
         if len(self._holds) > 14:
             raise Exception("Too many holds")
         # Mini Moonboard will need different criteria
-        if re.match("[A-K]([1-9]|(1[0-8]))", hold[0]) != hold[0]:
-            result = re.match("[A-K]([1-9]|(1[0-8]))", hold[0])
-            raise Exception("Incorrect location format. Value is " + result)
+        if re.match("[A-K]([1-9]|(1[0-8]))", hold[0]).group(0) != hold[0]:
+            raise Exception("Incorrect location format. Value is " + hold[0])
         self._holds.append(hold)
         if (hold[1]):
             self._start_holds += 1
