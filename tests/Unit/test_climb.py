64c83d83dbc993d2707c0eb3ca33585d018ce41c
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-12 21:24:50 -0400

    Update test_climb.py

--- tests/Unit/test_climb.py
+++ tests/Unit/test_climb.py
@@ -21,6 +21,7 @@ def test_factory() -> None:
     }''')
     climb = Climb.from_json("1", data)
     assert climb.get_name() == "Foo"
+    assert climb.url() = "https://moonboard.com/Problems/View/1/foo"
 
 
 # Setters and Getters
