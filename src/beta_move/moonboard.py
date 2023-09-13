9df6548667f28c8c4807a075814bc5533a19a69e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-13 13:09:44 -0400

    Update moonboard.py

--- src/beta_move/moonboard.py
+++ src/beta_move/moonboard.py
@@ -26,12 +26,29 @@ class Moonboard
         Return the features for the hold at a particular location
         """
 
-    def get_rh_difficulty((self: T, position: list) -> int:
+    def get_rh_difficulty(self: T, position: list) -> int:
         """
         Return the right hand difficulty for the hold at a particular location
         """
 
-    def get_lh_difficulty((self: T, position: list) -> int:
+    def get_lh_difficulty(self: T, position: list) -> int:
         """
         Return the left hand difficulty for the hold at a particular location
         """
+
+    def hold_exists(self: T, position: list) -> bool:
+        """
+        Check is a hold is present on the board at a given lication
+        """
+
+    def height(self: T) -> int:
+        """
+        How tall is the board
+        """
+       return 18
+
+    def width(self: T) -> int:
+        """
+        How wide is the board
+        """
+        return 11
