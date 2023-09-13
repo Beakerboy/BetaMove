84dae4775d6ec0af282da38abb54de3abf756e05
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-13 13:27:30 -0400

    Update test_moonboard.py

--- tests/Unit/test_moonboard.py
+++ tests/Unit/test_moonboard.py
@@ -5,3 +5,13 @@ from beta_move.moonboard import Moonboard
 def test_constructor() -> None:
     board = Moonboard()
     assert isinstance(board, Moonboard)
+
+
+def test_height() -> None:
+    board = Moonboard()
+    assert board.height == 18
+
+
+def test_width() -> None:
+    board = Moonboard()
+    assert board.width == 110
