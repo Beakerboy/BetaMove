819e8539c268c6901ab5d5b2e7910df2e1a7d7b1
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-15 15:32:04 -0400

    Update beta_move.py

--- src/beta_move/beta_move.py
+++ src/beta_move/beta_move.py
@@ -19,11 +19,12 @@ class BetaMove:
         # movement = []
         if climb.is_valid:
             i = 0
+            x_vectors = np.zeros((10, climb.num_holds()))
             for (x, y) in climb.get_holds():
-                x_vectors = np.zeros((10, climb.num_holds()))
                 x_vectors[0:6, i] = self._board.get_features((x, y))
                 x_vectors[6:8, i] = [x, y]
-                x_vectors[8:, 0:climb.num_starts()] = np.array([[1], [0]])
-                x_vectors[8:, climb.num_holds() - climb.num_finish():] = np.array([[0], [1]])
                 i += 1
+            x_vectors[8:, 0:climb.num_starts()] = np.array([[1], [0]])
+            x_vectors[8:, climb.num_holds() - climb.num_finish():] = np.array([[0], [1]])
+
         return x_vectors
