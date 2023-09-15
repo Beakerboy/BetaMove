df05f5ee392b1ccccf8069c1a2d6dfff4f057e98
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-14 21:02:03 -0400

    Update test_beta_move.py

--- tests/Unit/test_beta_move.py
+++ tests/Unit/test_beta_move.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from beta_move.beta_move import BetaMove
 from beta_move.climb import Climb
+from bet_move.moonboard import Moonboard
 
 
 def test_constructor() -> None:
@@ -11,39 +12,7 @@ def test_constructor() -> None:
     assert isinstance(app, BetaMove)
 
 
-def test_lh_set() -> None:
-    file = "data/hold_features_2016_LH.csv"
-    features = pd.read_csv(file, dtype=str)
-    dict = {}
-    for index in features.index:
-        item = features.loc[index]
-        dict[
-            (int(item['X_coord']), int(item['Y_coord']))
-        ] = np.array(list(item['Difficulties'])).astype(int)
-    app = BetaMove(dict, dict)
-    left = app.get_left()
-    assert len(left) == 140
-    assert left[(0, 4)] == [4]
-    assert left[(0, 10)] == [1]
-
-
 def test_status() -> None:
-    file = "data/hold_features_2016_LH.csv"
-    features = pd.read_csv(file, dtype=str)
-    lh = {}
-    for index in features.index:
-        item = features.loc[index]
-        lh[
-            (int(item['X_coord']), int(item['Y_coord']))
-        ] = np.array(list(item['Difficulties'])).astype(int)
-    file = "data/hold_features_2016_RH.csv"
-    features = pd.read_csv(file, dtype=str)
-    rh = {}
-    for index in features.index:
-        item = features.loc[index]
-        rh[
-            (int(item['X_coord']), int(item['Y_coord']))
-        ] = np.array(list(item['Difficulties'])).astype(int)
 
     expected = [
         [5., 4., 9., 4., 1., 1., 5., 4., 1., 0.],
@@ -56,10 +25,9 @@ def test_status() -> None:
         [2., 5., 2., 1., 0., 0., 1., 15., 0., 0.],
         [2., 6., 8., 6., 2., 0., 3., 17., 0., 1.]
     ]
-    app = BetaMove(lh, rh)
-    assert app == app
+    board = Moonboard(2016)
+    app = BetaMove(board)
     f = open('tests/Unit/342797.json')
     data = json.load(f)
     climb = Climb.from_json("342797", data["342797"])
-    assert expected == expected
-    assert climb == climb
+    assert app.movement(climb) == expected
