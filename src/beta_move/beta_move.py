3f2da80fe86fbc1284fe5415326806d43ab32867
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-15 08:13:34 -0400

    Update beta_move.py

--- src/beta_move/beta_move.py
+++ src/beta_move/beta_move.py
@@ -1,3 +1,4 @@
+import numpy as np
 from beta_move.climb import Climb
 from beta_move.moonboard import Moonboard
 from typing import TypeVar
@@ -14,5 +15,14 @@ class BetaMove:
         # Instance Attributes
 
     def create_movement(self: T, problem: Climb) -> list:
-        movement = []
-        return movement
+        # movement = []
+        if climb.is_valid:
+            for i, (x, y) in enumerate(climb.get_holds()):
+                x_vectors = np.zeros((10, n_hold))
+                x_vectors[0:6, i] = board.get_features((x, y)) # 6 hand features
+                x_vectors[6:8, i] = [x, y] #(x, y)
+                # for each hold set whether it is start/end based on index (as combined_list is start:mid:end)
+                x_vectors[8:, 0:climb.num_starts()] = np.array([[1], [0]])
+                x_vectors[8:, climb.num_holds() - climb.num_finish():] = np.array([[0], [1]])
+
+        return x_vectors
