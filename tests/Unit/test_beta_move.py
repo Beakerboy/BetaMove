8c6793e1bfa0d93c28a9d744eac6c682065a6eae
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-19 22:27:33 -0400

    Update test_beta_move.py

--- tests/Unit/test_beta_move.py
+++ tests/Unit/test_beta_move.py
@@ -55,4 +55,4 @@ def test_status() -> None:
 def test_pickle_data() -> None:
     f = open("tests/pickle_data/benchmark_withgrade_move_seq_X", "rb")
     data = pickle.load(f)
-    assert data["99677"] == {}, str(data)
+    assert data["99677"] == {}, f"Data is {data}"
