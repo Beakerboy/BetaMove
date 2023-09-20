e563db759542557cdbf1174ebb44f1221b476d03
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-20 05:40:21 -0400

    Update test_beta_move.py

--- tests/Unit/test_beta_move.py
+++ tests/Unit/test_beta_move.py
@@ -14,6 +14,26 @@ def test_constructor() -> None:
     assert isinstance(app, BetaMove)
 
 
+def test_match_hokd_features() -> None:
+    expected = [
+        [5., 4., 9., 4., 1., 1., 5., 4., 1., 0.],
+        [0., 2., 4., 2., 0., 0., 4., 7., 0., 0.],
+        [1., 5., 2., 0., 0., 0., 0., 8., 0., 0.],
+        [0., 0., 4., 9., 4., 0., 7., 9., 0., 0.],
+        [0., 0., 6., 4., 4., 0., 3., 11., 0., 0.],
+        [0., 3., 6., 3., 0., 0., 4., 12., 0., 0.],
+        [1., 4., 3., 1., 0., 0., 2., 14., 0., 0.],
+        [2., 5., 2., 1., 0., 0., 1., 15., 0., 0.],
+        [2., 6., 8., 6., 2., 0., 3., 17., 0., 1.]
+    ]
+    board = Moonboard(2016)
+    app = BetaMove(board)
+    f = open('tests/Unit/342797.json')
+    data = json.load(f)
+    climb = Climb.from_json("342797", data["342797"])
+    np.testing.assert_array_equal(app.match_hold_features(climb).T, expected)
+
+
 gauss_data = [
     [[[5, 6], [4, 5], "LH"], .37802090861230714],
     [[[3, 6], [4, 5], "RH"], .37802090861230714],
