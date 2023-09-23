427124bc76fe28e5de28aa6dc3bd8485df562a9f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-23 10:22:44 -0400

    Update test_beta_move.py

--- tests/Unit/test_beta_move.py
+++ tests/Unit/test_beta_move.py
@@ -13,7 +13,7 @@ def test_constructor() -> None:
     assert isinstance(app, BetaMove)
 
 
-def test_match_hokd_features() -> None:
+def test_match_hold_features() -> None:
     expected = [
         [5., 4., 9., 4., 1., 1., 5., 4., 1., 0.],
         [0., 2., 4., 2., 0., 0., 4., 7., 0., 0.],
@@ -31,7 +31,27 @@ def test_match_hokd_features() -> None:
     data = json.load(f)
     climb = Climb.from_json("342797", data["342797"])
     np.testing.assert_array_equal(app.match_hold_features(climb).T, expected)
-    np.testing.assert_array_equal(app.get_all_holds().T, expected)
+
+
+def test_get_all() -> None:
+    expected = np.array([
+        [5., 4., 9., 4., 1., 1., 5., 4., 1., 0.],
+        [0., 2., 4., 2., 0., 0., 4., 7., 0., 0.],
+        [1., 5., 2., 0., 0., 0., 0., 8., 0., 0.],
+        [0., 0., 4., 9., 4., 0., 7., 9., 0., 0.],
+        [0., 0., 6., 4., 4., 0., 3., 11., 0., 0.],
+        [0., 3., 6., 3., 0., 0., 4., 12., 0., 0.],
+        [1., 4., 3., 1., 0., 0., 2., 14., 0., 0.],
+        [2., 5., 2., 1., 0., 0., 1., 15., 0., 0.],
+        [2., 6., 8., 6., 2., 0., 3., 17., 0., 1.]
+    ]).T
+    board = Moonboard(2016)
+    app = BetaMove(board)
+    f = open('tests/Unit/342797.json')
+    data = json.load(f)
+    climb = Climb.from_json("342797", data["342797"])
+    app.create_movement(climb)
+    np.testing.assert_array_equal(app.get_all_holds(), expected)
 
 
 gauss_data = [
