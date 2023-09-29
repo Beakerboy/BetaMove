a9ab2202562e496791edbf01b5e9e7cc58ec7178
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-09-29 09:46:43 -0400

    Update test_beta_move.py

--- tests/Unit/test_beta_move.py
+++ tests/Unit/test_beta_move.py
@@ -50,7 +50,7 @@ def test_create_movement() -> None:
     expected = {
         "hold_index": [0, 0, 1, 3, 4, 5, 7, 8],
         "hands": ['LH', 'RH', 'LH', 'RH', 'LH', 'RH', 'LH', 'RH'],
-        "success": 98.50396893
+        "success": 98.503968934466
     }
     board = Moonboard(2016)
     app = BetaMove(board)
