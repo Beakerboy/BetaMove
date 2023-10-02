cb6dbf785fb1c7c15b8a7d54c54b40ab2668989b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-02 10:20:30 -0400

    Update test_beta_move.py

--- tests/Unit/test_beta_move.py
+++ tests/Unit/test_beta_move.py
@@ -118,15 +118,19 @@ def test_all() -> None:
     all_results = pickle.load(f2)
     failures = []
     missing = []
+    exceptions = []
     for key in all_climbs:
-        climb = Climb.from_old_json(key, all_climbs[key])
-        if key in all_results:
-            expected = all_results['X_dict_seq'][key]
-            result = app.process_data(climb)
-            if not np.array_equal(result, expected[0:3]):
-                failures.append(key)
-        else:
-            missing.append(key)
+        try:
+            climb = Climb.from_old_json(key, all_climbs[key])
+            if key in all_results:
+                expected = all_results['X_dict_seq'][key]
+                result = app.process_data(climb)
+                if not np.array_equal(result, expected[0:3]):
+                    failures.append(key)
+            else:
+                missing.append(key)
+        except:
+            exception.append(key)
     assert len(failures) == 0, \
         f'{len(failures)} out of {len(all_climbs)} failed: {failures}'
 
