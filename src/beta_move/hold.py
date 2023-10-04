584eb8cb9f358955e971355a12e964e2d2045aed
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-04 10:24:00 -0400

    Update hold.py

--- src/beta_move/hold.py
+++ src/beta_move/hold.py
@@ -10,16 +10,35 @@ class Hold:
     def __init__(self: T, id: int) -> None:
         # Instance Attributes
         self._id = id
+        self._location = (0, 0)
+
+        # (left, right)
+        self._difficulties = (0, 0)
+        self._features = (0, 0, 0, 0, 0, 0)
 
     def get_difficulties(self: T) -> tuple:
-        pass
+        return self._difficulties
 
     def get_right_difficulty(self: T) -> int:
-        pass
+         """
+        return the right hand difficulty
+        """
+        return self._difficulties[1]
 
     def get_left_difficulty(self: T) -> int:
-        pass
+        """
+        return the left hand difficulty
+        """
+        return self._difficulties[0]
 
     def get_features(self: T) -> tuple:
-        pass
+        """
+        return the features
+        """
+        return self._features
     
+    def distance_to(self: T, hold: T) -> float:
+        """
+        Calculate the distance between this hold and another hold
+        """
+        pass
