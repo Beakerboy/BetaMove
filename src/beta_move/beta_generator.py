f6eaa99bcebe075a57170ccf4f37f5cec122c40f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-04 13:34:32 -0400

    Update beta_generator.py

--- src/beta_move/beta_generator.py
+++ src/beta_move/beta_generator.py
@@ -11,7 +11,7 @@ T = TypeVar('T', bound='BetaGenerator')
 
 
 class BetaGenerator:
-    
+
     @classmethod
     def create_movement(cls: Type[T], climb: Climb) -> BetaMove:
         board = Moonboard()
