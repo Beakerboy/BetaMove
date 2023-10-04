3880e37a3ebc70904c5e6d715da91fab426a52ac
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-04 10:18:39 -0400

    Update hold.py

--- src/beta_move/hold.py
+++ src/beta_move/hold.py
@@ -1,1 +1,25 @@
+from typing import TypeVar, Type
 
+
+T = TypeVar('T', bound='Hold')
+
+
+class Hold:
+
+    # class default constructor
+    def __init__(self: T, id: int) -> None:
+        # Instance Attributes
+        self._id = id
+
+    def get_difficulties(self: T) -> tuple:
+        pass
+
+    def get_right_difficulty(self: T) -> int:
+        pass
+
+    def get_left_difficulty(self: T) -> int:
+        pass
+
+    def get_features(self: T) -> tuple:
+        pass
+    
