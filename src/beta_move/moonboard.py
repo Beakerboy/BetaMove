7f06b0485299f6be49c21829b60de8dc38e0257b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-04 09:01:28 -0400

    Update moonboard.py

--- src/beta_move/moonboard.py
+++ src/beta_move/moonboard.py
@@ -26,9 +26,12 @@ class Moonboard:
         self._height: int = 18
         if year == 2016:
             base_path = pathlib.Path(__file__).parent
-            self._lh = self._transform2((base_path / "../data/hold_features_2016_LH.csv").resolve())
-            self._rh = self._transform2((base_path / "../data/hold_features_2016_RH.csv").resolve())
-            self._features = self._transform((base_path / "../data/hold_features.csv").resolve())
+            path = (base_path / "../data/hold_features_2016_LH.csv").resolve()
+            self._lh = self._transform2(path)
+            path = (base_path / "../data/hold_features_2016_RH.csv").resolve()
+            self._rh = self._transform2(path)
+            path = (base_path / "../data/hold_features.csv").resolve()
+            self._features = self._transform(path)
 
     def get_features(self: T, position: Tuple[int, int]) -> np.ndarray:
         """
