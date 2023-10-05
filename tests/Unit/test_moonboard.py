d587dc3bb31e844af2019bc2b954e5d83408e8ea
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-05 12:37:03 -0400

    Update test_moonboard.py

--- tests/Unit/test_moonboard.py
+++ tests/Unit/test_moonboard.py
@@ -1,3 +1,4 @@
+import pytest
 from beta_move.moonboard import Moonboard
 
 
@@ -32,14 +33,29 @@ def test_get_features() -> None:
     assert all(board.get_features((5, 4)) == [5, 4, 9, 4, 1, 1])
 
 
-def test_lh_difficulty() -> None:
-    board = Moonboard()
-    assert board.get_lh_difficulty((0, 17)) == 5
+difficulty_data = [
+    [(5, 4), [8, 7]],
+    [(4, 7), [8, 8]],
+    [(0, 8), [8, 3]],
+    [(7, 9), [8, 9]],
+    [(3, 11), [6, 7]],
+    [(4, 12), [7, 7]],
+    [(2, 14), [4, 2]],
+    [(1, 15), [5, 3]],
+    [(3, 17), [9, 9]],
+]
 
 
-def test_rh_difficulty() -> None:
+@pytest.mark.parametrize("input, expected", difficulty_data)
+def test_lh_difficulty(input: tuple, difficulty: list) -> None:
     board = Moonboard()
-    assert board.get_rh_difficulty((0, 15)) == 2
+    assert board.get_lh_difficulty(input) == difficulty[0]
+
+
+@pytest.mark.parametrize("input, expected", difficulty_data)
+def test_rh_difficulty(input: tuple, difficulty: list) -> None:
+    board = Moonboard()
+    assert board.get_rh_difficulty(input) == difficulty[1]
 
 
 def test_position_to_location() -> None:
