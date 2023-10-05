497cf198c3c3f4b791a8620dea665475ef420ec4
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-05 12:18:57 -0400

    Update test_moonboard.py

--- tests/Unit/test_moonboard.py
+++ tests/Unit/test_moonboard.py
@@ -41,6 +41,6 @@ def test_rh_difficulty() -> None:
     board = Moonboard()
     assert board.get_rh_difficulty((0, 15)) == 2
 
+
 def test_position_to_location() -> None:
     assert Moonboard.position_to_location('A1') == (0, 0)
-
