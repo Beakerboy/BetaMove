22e186c5d5963987c52c467e9430db5b7bf5d4cd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-05 14:41:52 -0400

    Update main.py

--- src/beta_move/main.py
+++ src/beta_move/main.py
@@ -4,7 +4,6 @@ import numpy as np
 import pandas as pd
 from beta_move.beta_generator import BetaGenerator
 from beta_move.climb import Climb
-from beta_move.moonboard import Moonboard
 
 
 def main() -> None:
@@ -32,7 +31,7 @@ def main() -> None:
                 list(item['Difficulties'])
             ).astype(int)
         return problem
-    
+
     # Load the json file
     f = open(args.filename)
     data = json.load(f)
