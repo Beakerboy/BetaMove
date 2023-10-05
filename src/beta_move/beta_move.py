6d6299aa818dc6c4c1f52923b25c79fad5b405dd
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-05 13:55:36 -0400

    Update beta_move.py

--- src/beta_move/beta_move.py
+++ src/beta_move/beta_move.py
@@ -86,42 +86,7 @@ class BetaMove:
             raise Exception("Climb is invalid.")
         return x_vectors
 
-    def create_movement(self: T, climb: Climb) -> T:
-        # reset onject state
-        self.reset_state()
-        # movement = []
-        x_vectors = self.match_hold_features(climb)
-        self.allHolds = x_vectors.T
-        self.totalNumOfHold = np.size(x_vectors.T, axis=0)
-        self.holdsNotUsed = list(range(self.totalNumOfHold))
-        beta1 = copy.deepcopy(self)
-        beta2 = copy.deepcopy(self)
-        status = [beta1, beta2]
-        status[0].add_start_holds(False)
-        status[1].add_start_holds(True)
-        total_run = status[0].totalNumOfHold - 1
-
-        for i in range(total_run):
-            status = BetaMove.add_new_beta(status, False)
-            final_score = []
-            for j in status:
-                final_score.append(j.overall_success_rate())
-                iter = range(len(final_score))
-                key_func = final_score.__getitem__
-                largest_index = heapq.nlargest(4, iter, key=key_func)
-                comp1 = status[largest_index[0]].isFinished
-                if comp1 and status[largest_index[1]].isFinished:
-                    break
-        final_score = []
-        for j in status:
-            final_score.append(j.overall_success_rate())
-        iter = range(len(final_score))
-        key_func = final_score.__getitem__
-        largest_index = heapq.nlargest(1, iter, key=key_func)
-        return status[largest_index[0]]
-
     def process_data(self: T, climb: Climb) -> np.ndarray:
-        movement = self.create_movement(climb)
         output = np.vstack([
             self.allHolds.T[6:8, movement.handSequence],
             ((np.array(movement.handOperator) == 'LH') * (-1)
@@ -359,58 +324,6 @@ class BetaMove:
 
     def set_true_beta(self: T) -> None:
         self.isTrueBeta = True
-
-    @classmethod
-    def add_new_beta(cls: Type[T],
-                     status: list,
-                     print_out: bool = True
-                     ) -> list:
-        """
-        Add one move to expand the candidate list and pick the largest 8
-        """
-        tempstatus = []
-        for beta_pre in status:
-            distance_scores = []
-            hyperparameter = [1, 1]
-            for next_hold_order in beta_pre.holdsNotUsed:
-                original_com = np.array(beta_pre.get_current_com())
-                hyper_0 = hyperparameter[0]
-                success_rate = beta_pre.last_move_success_rate_by_hold()
-                dynamic_threshold = hyper_0 * success_rate
-                final_coor = beta_pre.get_xy_from_order(next_hold_order)
-                final_xy = np.array(final_coor)
-                distance = np.linalg.norm(original_com - final_xy)
-                # evaluate success rate simply consider the distance
-                # (not consider left and right hand)
-                success = beta_pre.success_rate_by_distance(
-                    distance,
-                    dynamic_threshold
-                )
-                distance_scores.append(success)
-            # Find the first and second smallest distance in the distance_score
-            num = min(8, len(distance_scores))
-            iter = range(len(distance_scores))
-            key_name = distance_scores.__getitem__
-            largest_index = heapq.nlargest(num, iter, key=key_name)
-
-            good_hold_index = [beta_pre.holdsNotUsed[i] for i in largest_index]
-            added = False
-            for possible_hold in good_hold_index:
-                for op in ["RH", "LH"]:
-                    if not beta_pre.isFinished:
-                        tempstatus.append(copy.deepcopy(beta_pre))
-                        tempstatus[-1].add_next_hand(possible_hold, op)
-                    elif not added:
-                        tempstatus.append(copy.deepcopy(beta_pre))
-                        added = True
-
-        # trim tempstatus to pick the largest 8
-        final_score = []
-        for i in tempstatus:
-            final_score.append(i.overall_success_rate())
-        iter = range(len(final_score))
-        largest_index = heapq.nlargest(8, iter, key=final_score.__getitem__)
-        return [tempstatus[i] for i in largest_index]
 
     @classmethod
     def make_gaussian(
