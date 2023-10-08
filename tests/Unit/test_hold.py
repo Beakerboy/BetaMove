994a0bc629f79bdf6794ed27f75b69ab47ebc278
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-07 22:59:41 -0400

    Update test_hold.py

--- tests/Unit/test_hold.py
+++ tests/Unit/test_hold.py
@@ -25,3 +25,9 @@ def test_get_x() -> None:
 def test_get_y() -> None:
     hold = Hold("0", (0, 2), 0)
     assert hold.get_y() == 2
+
+
+def test_distance_to() -> None:
+    hold1 = Hold("0", (0, 0), 0)
+    hold2 = Hold("0", (3, 4), 0)
+    assert hold1.distance_to(hold2) == 5.0
