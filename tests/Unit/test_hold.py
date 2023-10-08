7982fa849c616979fe2a7af885d15834d72dddd0
Kevin Nowaczyk <beakerboy99@yahoo.com>
2023-10-07 22:49:08 -0400

    Update test_hold.py

--- tests/Unit/test_hold.py
+++ tests/Unit/test_hold.py
@@ -3,5 +3,25 @@ from beta_move.hold import Hold
 
 # Construction
 def test_constructor() -> None:
-    hold = Hold("0")
+    hold = Hold("0", (0, 0), 0)
     assert isinstance(hold, Hold)
+
+
+def test_get_location() -> None:
+    hold = Hold("0", (0, 0), 0)
+    assert hold.get_location() == (0, 0)
+
+
+def test_get_position() -> None:
+    hold = Hold("0", (0, 0), 0)
+    assert hold.get_position() == "A1"
+
+
+def test_get_x() -> None:
+    hold = Hold("0", (0, 0), 0)
+    assert hold.get_x() == 0
+
+
+def test_get_y() -> None:
+    hold = Hold("0", (0, 2), 0)
+    assert hold.get_y() == 2
